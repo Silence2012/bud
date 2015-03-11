cbb25d438ed6ef7107bcb789c63d72a53225dfab
SimonSun <386488135@qq.com>
2015-03-11 18:23:15 +0800

    "del some unuseful files "

--- build/bdist.linux-x86_64/rpm/BUILD/monitor-agent-0.1.1-dev/setup.py
+++
@@ -1,9 +0,0 @@
-from distutils.core import setup
-setup(
-name = 'monitor-agent',
-author = 'SimonSun',
-author_email = '386488135@qq.com',
-description = 'a monitor agent',
-version = '0.1.1-dev',
-packages = ['agent','web'],
-)
