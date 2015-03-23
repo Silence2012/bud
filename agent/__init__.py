e33fdd6a0ec9fba78df64287dca33e44426a8038
SimonSun <386488135@qq.com>
2015-03-23 19:53:57 +0800

    add the host cpu metric

--- agent/__init__.py
+++ agent/__init__.py
@@ -1,8 +1,9 @@
 import tornado.ioloop
 import tornado.web
 import json
-from handle import Handle
-from container import Container
+from container.handle import Handle
+from container.container import Container
+from host.cpu import Cpuinfo
 class getContainerArg(tornado.web.RequestHandler):
     def get(self):
         self.handle = Handle()
@@ -22,30 +23,51 @@ class MainHandler(tornado.web.RequestHandler):
     def get(self):
         self.handle =Handle()
         self.write(json.dumps(self.handle.getMetric()))
-class getCpu(tornado.web.RequestHandler):
-    def get(self):
-        self.handle =Handle()
-        self.write(json.dumps(self.handle.getCpu()))
-class getMem(tornado.web.RequestHandler):
-    def get(self):
+class getMetric(tornado.web.RequestHandler):
+    def get(self,name):
         self.handle = Handle()
-        self.write(json.dumps(self.handle.getMem()))
-class getBlk(tornado.web.RequestHandler):
-    def get(self):
+        lt = ["cpu","mem","blkio","net"]
+        if name in lt:
+            if name == "cpu":
+                ret = self.handle.getCpu()
+            elif name == "mem":
+                ret = self.handle.getMem()
+            elif name == "blkio":
+                ret = self.handle.getBlk()
+            else :
+                ret = self.handle.getNet()
+        self.write(json.dumps(ret))
+class getbyIdMetric(tornado.web.RequestHandler):
+    def get(self,id,name):
         self.handle = Handle()
-        self.write(json.dumps(self.handle.getBlk()))
-class getNet(tornado.web.RequestHandler):
-    def get(self):
-        self.handle = Handle()
-        self.write(json.dumps(self.handle.getNet()))
+        self.con = Container()
+        for item in self.con.getContainerTrucId():
+            if id == item:
+                if name == "cpu":
+                     ret = self.handle.getCpu()[id]
+                if name == "mem":
+                     ret = self.handle.getMem()[id]
+                if name == "blkio":
+                     ret = self.handle.getBlk()[id]
+                if name == "net":
+                     ret = self.handle.getNet()[id]
+        self.write(json.dumps(ret))
+class getHost(tornado.web.RequestHandler):
+    def get (self,name):
+        self.ins = Cpuinfo()
+        if name == "cpu":
+            ret = self.ins.getData()
+        else :
+            ret = "Waiting for a moment "
+        self.write(json.dumps(ret))
+                
 app= tornado.web.Application([
 (r"/v1/",MainHandler),
 (r"/v1/runContainerId",ContainerHandler),
 (r"/v1/getArg",getContainerArg),
-(r"/v1/container/cpu",getCpu),
-(r"/v1/container/mem",getMem),
-(r"/v1/container/blkio",getBlk),
-(r"/v1/container/net",getNet),
+(r"/v1/container/([a-zA-Z]+)",getMetric),
+(r"/v1/container/([0-9a-zA-Z]+)/([a-zA-Z]+)",getbyIdMetric),
+(r"/v1/host/([a-z]+)",getHost),
 ])
 if __name__ == "__main__":
     app.listen(8888)
