d75bbe4d0fcc8c0d5f01280f081124c191880901
SimonSun <386488135@qq.com>
2015-03-25 14:17:28 +0800

    0.1.3 rc1

--- agent/__init__.py
+++ agent/__init__.py
@@ -4,22 +4,45 @@ import json
 from container.handle import Handle
 from container.container import Container
 from host.host import Hostinfo
+
 class getContainerArg(tornado.web.RequestHandler):
-    def get(self):
+    """
+    get the container id which is running on the host
+
+    """
+    def get(self, id):
         self.handle = Handle()
         self.con = Container()
         self.dt = {}
+	"""
+	we have a another wonderful method to make
+	things better.
+        """
+	"""
         for item in self.con.runContainerId():
             self.ret = self.con.getContainerArg(item)
             self.dt[item]= self.ret
         self.write(json.dumps(self.dt))
-class ContainerHandler(tornado.web.RequestHandler):
+	
+	"""
+        self.ret = self.con.getContainerArg(id)
+	self.dt[id] = self.ret
+	self.write(json.dumps(self.dt))
+
+	
+class runContainerId(tornado.web.RequestHandler):
     def get(self):
         self.handle = Handle()
         self.con = Container()
+	"""
+	changed the runContainerid with getContainerTrucId
+	"""
+	"""
         self.ret_con = self.con.runContainerId()
+	"""
+	self.ret_con = self.con.getContainerTrucId()
         self.write(json.dumps(self.ret_con))
-class MainHandler(tornado.web.RequestHandler):
+class getAllMetrics(tornado.web.RequestHandler):
     def get(self):
         self.handle =Handle()
         self.write(json.dumps(self.handle.getMetric()))
@@ -68,9 +91,9 @@ class getHost(tornado.web.RequestHandler):
         self.write(json.dumps(ret))
                 
 app= tornado.web.Application([
-(r"/v1/",MainHandler),
-(r"/v1/runContainerId",ContainerHandler),
-(r"/v1/getArg",getContainerArg),
+(r"/v1/container/metrics",getAllMetrics),
+(r"/v1/containers",runContainerId),
+(r"/v1/container/([0-9a-zA-Z]+)/arg",getContainerArg),
 (r"/v1/container/([a-zA-Z]+)",getMetric),
 (r"/v1/container/([0-9a-zA-Z]+)/([a-zA-Z]+)",getbyIdMetric),
 (r"/v1/host/([a-z]+)",getHost),
