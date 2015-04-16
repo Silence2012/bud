1b780d7feadd64d88928fad1e44b979924467cac
SimonSun <386488135@qq.com>
2015-04-16 10:49:56 +0800

    "Add some optimization to make the service as a container

--- scripts/docker_run.sh
+++ scripts/docker_run.sh
@@ -1,1 +1,1 @@
-docker run -itd --privileged --name "bud" -v /cgroup:/cgroup -v /proc:/proc -P moon_image:v1
+docker run -itd --privileged --name "bud" -v /var/run/netns:/var/run/netns -v /cgroup:/cgroup -v /proc:/proc -v /var/run/docker.sock:/var/run/docker.sock -P moon_image:v1
